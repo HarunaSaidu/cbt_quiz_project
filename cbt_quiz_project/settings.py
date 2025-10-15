bbbe71f215ccd752870d2ec0f298520517c4fce6
haruna <mohammadsaiduharuna@gmail.com>
2025-10-15 13:07:45 +0100

    Try fix

--- cbt_quiz_project/settings.py
+++ cbt_quiz_project/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'cbt_quiz_project.wsgi.app'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db()
+    # 'default': env.db()
 }
 
 
